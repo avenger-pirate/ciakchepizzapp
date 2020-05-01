3e189ec7b7e2902c5017e91a4e0e33a3bf101aac
black-jack <black-jack@Black-Jack.Black-Jack>
2020-05-01 21:17:38 +0200

    01/05/2020 - ciakchepizzapp - primo commit

---
+++ CiakChePizza/app/src/main/java/com/Pizzeria/ciakchepizza/GestioneAccount.java
@@ -0,0 +1,14 @@
+package com.Pizzeria.ciakchepizza;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GestioneAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gestione_account);
+    }
+}
---
+++ CiakChePizza/app/src/main/java/com/Pizzeria/ciakchepizza/Paga.java
@@ -0,0 +1,14 @@
+package com.Pizzeria.ciakchepizza;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Paga extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paga);
+    }
+}
