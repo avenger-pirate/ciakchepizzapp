8e875e54d28adee465ab2f75ef00ccc983489c89
black-jack <black-jack@Black-Jack.Black-Jack>
2020-05-26 15:32:32 +0200

    prova2 - 26/05/2020 - 15.32

--- APP PROVA PAGINA WEB/prova2/app/src/main/java/org/altervista/mycloud6/prova2/browser.java
+++ APP PROVA PAGINA WEB/prova2/app/src/main/java/org/altervista/mycloud6/prova2/browser.java
@@ -1,8 +1,10 @@
 package org.altervista.mycloud6.prova2;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,7 +18,33 @@ public class browser extends AppCompatActivity {
 
         webView = (WebView) findViewById(R.id.Browser);
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl("http://ciakchepizza.com/m/formLogin.php");
+
+        int screenSize = getResources().getConfiguration().screenLayout &
+                Configuration.SCREENLAYOUT_SIZE_MASK;
+
+        String toastMsg;
+        switch(screenSize) {
+            case Configuration.SCREENLAYOUT_SIZE_LARGE:
+                toastMsg = "Large screen";
+                webView.loadUrl("https://ciakchepizza.com");
+                break;
+            case Configuration.SCREENLAYOUT_SIZE_NORMAL:
+                toastMsg = "Normal screen";
+                webView.loadUrl("https://ciakchepizza.com/m/formLogin.php");
+                break;
+            case Configuration.SCREENLAYOUT_SIZE_SMALL:
+                toastMsg = "Small screen";
+                webView.loadUrl("https://ciakchepizza.com/m/formLogin.php");
+                break;
+            default:
+                toastMsg = "Le dimensioni dello schermo non sono né large, ne normal, ne piccole";
+                webView.loadUrl("https://ciakchepizza.com/m/tabletFormLogin.php");
+
+        }
+        Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show();
+
+
+        //webView.loadUrl("https://ciakchepizza.com/m/formLogin.php");
 
 
     }
